2a52a6ba20222c97c72670d59f8c1a2c36cd3391
jinchlu <jincheng1313@163.com>
2021-09-20 05:12:25 +0800

    项目初建

--- src/main/java/com/robret/lujc/cn/redis/RedisWindowFactory.java
+++ src/main/java/com/robret/lujc/cn/redis/RedisWindowFactory.java
@@ -1,11 +1,14 @@
-package com.robret.lujc.cn.redis.ui;
+package com.robret.lujc.cn.redis;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import com.robret.lujc.cn.redis.ui.RedisFrame;
 import org.jetbrains.annotations.NotNull;
+
+import java.sql.SQLOutput;
 
 /**
  * redis工具窗口
@@ -13,9 +16,10 @@ import org.jetbrains.annotations.NotNull;
 public class RedisWindowFactory implements ToolWindowFactory {
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
-        RedisWindow redisWindow = new RedisWindow(toolWindow);
+        System.out.println("=====================我的插件====================================");
+        RedisFrame redisFrame = new RedisFrame(toolWindow);
         ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
-        Content content = contentFactory.createContent(redisWindow.getContent(), "", false);
+        Content content = contentFactory.createContent(redisFrame.getContent(), "", false);
         toolWindow.getContentManager().addContent(content);
     }
 }
--- src/main/java/com/robret/lujc/cn/redis/ui/RedisFrame.java
+++ src/main/java/com/robret/lujc/cn/redis/ui/RedisFrame.java
@@ -1,2 +1,36 @@
-package com.robret.lujc.cn.redis.ui;public class RedisFrame {
+package com.robret.lujc.cn.redis.ui;
+
+import com.intellij.openapi.wm.ToolWindow;
+
+import javax.swing.*;
+
+public class RedisFrame {
+
+    /**
+     * 窗口相关功能
+     */
+    private ToolWindow toolWindow;
+
+    /**
+     * 主窗口
+     */
+    private JPanel mainWindow;
+
+    private JButton addRedisButton;
+
+
+    public RedisFrame(ToolWindow toolWindow) {
+        this.toolWindow = toolWindow;
+        mainWindow = new JPanel();
+        createMainWindow();
+    }
+
+    private void createMainWindow() {
+        addRedisButton = new JButton("增加");
+        mainWindow.add(addRedisButton);
+    }
+
+    public JComponent getContent() {
+        return mainWindow;
+    }
 }
